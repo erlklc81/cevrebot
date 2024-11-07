b5c444596e2e38a3647ca8962e6b7d1856dd48e4
erlklc81 <erlklc81@gmail.com>
2024-11-07 15:26:14 +0300

    Update cevre.py

--- cevre.py
+++ cevre.py
@@ -1,13 +1,14 @@
 import discord
 from discord.ext import commands
-
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
-
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
 @bot.command()
-async def sohbet(ctx):
-    await ctx.send(f'Merhaba {ctx.author.name}, çevre ile ilgili sohbet etmek ister misin?')
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
 
 @bot.command()
 async def neyapabilirim(ctx):
